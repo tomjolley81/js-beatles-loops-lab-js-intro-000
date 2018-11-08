0927c1ada1b493b66585ee397c22f47f020e042f
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 15:07:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = {}
+  for (var i = 0; i < array.length; i++) {
+    array[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+  return array
+}
+function johnLennonFacts() {
+
+}
+function iLoveTheBeatles() {
+  
+}
